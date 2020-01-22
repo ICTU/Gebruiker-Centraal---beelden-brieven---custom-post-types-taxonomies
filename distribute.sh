d1aafb7a9c8dc0f770cc11cfdf8330712b21f541
Paul van Buuren <paul@wbvb.nl>
2020-01-22 03:43:22 +0100

    admin

--- distribute.sh
+++ distribute.sh
@@ -1,59 +1,59 @@
 
-# sh '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/ictu-gc-posttypes-brieven-beelden/distribute.sh' &>/dev/null
+# sh '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/ictu-gc-posttypes-brieven-beelden/distribute.sh' &>/dev/null
 
 echo '----------------------------------------------------------------';
 echo 'Distribute GC post type plugin';
 
 # clear the log file
-> '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/debug.log'
+> '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/debug.log'
 
 # copy to temp dir
-rsync -r -a --delete '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/ictu-gc-posttypes-brieven-beelden/' '/shared-paul-files/Webs/temp/'
+rsync -r -a --delete '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/ictu-gc-posttypes-brieven-beelden/' '/Users/paul/shared-paul-files/Webs/temp/'
 
 # clean up temp dir
-rm -rf '/shared-paul-files/Webs/temp/.git/'
-rm '/shared-paul-files/Webs/temp/.gitignore'
-rm '/shared-paul-files/Webs/temp/config.codekit3'
-rm '/shared-paul-files/Webs/temp/distribute.sh'
-rm '/shared-paul-files/Webs/temp/README.md'
-rm '/shared-paul-files/Webs/temp/LICENSE'
+rm -rf '/Users/paul/shared-paul-files/Webs/temp/.git/'
+rm '/Users/paul/shared-paul-files/Webs/temp/.gitignore'
+rm '/Users/paul/shared-paul-files/Webs/temp/config.codekit3'
+rm '/Users/paul/shared-paul-files/Webs/temp/distribute.sh'
+rm '/Users/paul/shared-paul-files/Webs/temp/README.md'
+rm '/Users/paul/shared-paul-files/Webs/temp/LICENSE'
 
 
 # --------------------------------------------------------------------------------------------------------------------------------
 # Vertalingen --------------------------------------------------------------------------------------------------------------------
 # --------------------------------------------------------------------------------------------------------------------------------
 # remove the .pot
-rm '/shared-paul-files/Webs/temp/languages/ictu-gc-posttypes-brieven-beelden.pot'
+rm '/Users/paul/shared-paul-files/Webs/temp/languages/ictu-gc-posttypes-brieven-beelden.pot'
 
 # copy files to /wp-content/languages/themes
-rsync -ah '/shared-paul-files/Webs/temp/languages/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/languages/plugins/'
+rsync -ah '/Users/paul/shared-paul-files/Webs/temp/languages/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/languages/plugins/'
 
 # languages Sentia accept
-rsync -ah '/shared-paul-files/Webs/temp/languages/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/accept/www/wp-content/languages/plugins/'
+rsync -ah '/Users/paul/shared-paul-files/Webs/temp/languages/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/accept/www/wp-content/languages/plugins/'
 
 # languages Sentia live
-rsync -ah '/shared-paul-files/Webs/temp/languages/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/live/www/wp-content/languages/plugins/'
+rsync -ah '/Users/paul/shared-paul-files/Webs/temp/languages/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/live/www/wp-content/languages/plugins/'
 
 
 
 
 
-cd '/shared-paul-files/Webs/temp/'
+cd '/Users/paul/shared-paul-files/Webs/temp/'
 find . -name ‘*.DS_Store’ -type f -delete
 
 
 # copy from temp dir to dev-env
-#rsync -r -a --delete '/shared-paul-files/Webs/temp/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/ictu-gc-posttypes-brieven-beelden/' 
+#rsync -r -a --delete '/Users/paul/shared-paul-files/Webs/temp/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/ictu-gc-posttypes-brieven-beelden/' 
 
 # remove temp dir
-rm -rf '/shared-paul-files/Webs/temp/'
+rm -rf '/Users/paul/shared-paul-files/Webs/temp/'
 
 
 # een kopietje naar Sentia accept
-rsync -r -a --delete '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/ictu-gc-posttypes-brieven-beelden/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/accept/www/wp-content/plugins/ictu-gc-posttypes-brieven-beelden/'
+rsync -r -a --delete '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/ictu-gc-posttypes-brieven-beelden/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/accept/www/wp-content/plugins/ictu-gc-posttypes-brieven-beelden/'
 
 # en een kopietje naar Sentia live
-rsync -r -a --delete '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/ictu-gc-posttypes-brieven-beelden/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/live/www/wp-content/plugins/ictu-gc-posttypes-brieven-beelden/'
+rsync -r -a --delete '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/ictu-gc-posttypes-brieven-beelden/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/live/www/wp-content/plugins/ictu-gc-posttypes-brieven-beelden/'
 
 
 echo 'Ready';
